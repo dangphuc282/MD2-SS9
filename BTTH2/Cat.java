122bbce6304fa0bcb9e12ce9dfcb34597fa910b6
dangphuc282 <dangphuc28022002@gmail.com>
2025-07-30 14:06:01 +0900

    Add files via upload

---
+++ BTTH2/Cat.java
@@ -0,0 +1,14 @@
+public class Cat extends Pet{
+
+    public Cat() {
+    }
+
+    public Cat(String petId, String petName, int petAge) {
+        super(petId, petName, petAge);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Meo Meo");
+    }
+}
---
+++ BTTH2/Dog.java
@@ -0,0 +1,12 @@
+public class Dog extends Pet{
+    public Dog() {
+    }
+
+    public Dog(String petId, String petName, int petAge) {
+        super(petId, petName, petAge);
+    }
+    @Override
+    public void speak() {
+        System.out.println("Gau Gau");
+    }
+}
